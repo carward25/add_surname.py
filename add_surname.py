dea6776db8144f3707fc0560adab4e9bd04e5423
carward25 <83989145+carward25@users.noreply.github.com>
2021-11-03 15:09:55 -0700

    Update add_surname.py

--- add_surname.py
+++ add_surname.py
@@ -2,11 +2,11 @@
 #date: 11/3/2021
 #description: this function takes as a parameter a list of first names
 #then returns a list that contains only those names that start with a "K",
-# but with the surname "Kardashian" added to each one, with a space between the first and last names.
+#but with the surname "Kardashian" added to each one, with a space between the first and last names.
 
-def add_surname(name_list):
-   res = [name+' Kardashian' for name in name_list if name[0]=='K']
-   return res
+def add_surname(lst):
+    return [x+" Kardashian" for x in lst if x[0].lower()=='k']
 
-name_list = ["Kiki", "Krystal", "Pavel", "Annie", "Koala"]
-print(add_surname(name_list))
+# Testing
+lst = ["Kiki", "Krystal", "Pavel", "Annie", "Koala"]
+print(add_surname(lst))
